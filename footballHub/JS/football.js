748515980e31af512b8bf259156d03c008252f16
Adam Crofts <adamcrofts@live.com>
2020-04-21 13:22:46 +0100

    Button width fix

--- footballHub/JS/football.js
+++ footballHub/JS/football.js
@@ -14,7 +14,7 @@ function getTeams() {
     $.each(response.teams, function(index, list) {
       console.log(list.name);
       var url = "'teamMenu.html?teamId=" + list.id + "'";
-      $('#teamTable').append('<tr><td><button class="btn btn-jg btn-danger" onclick="window.location.href=' + url + '">' + list.name + '</button></td></tr>');
+      $('#teamTable').append('<tr><td><button class="btn btn-jg btn-danger" style="width: 275px;" onclick="window.location.href=' + url + '">' + list.name + '</button></td></tr>');
     });
   });
 }
