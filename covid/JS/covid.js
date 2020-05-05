2bd8c0ac31fc8807cf14bf83d338daf3d05a49d6
Adam Crofts <adamcrofts@live.com>
2020-05-05 13:23:48 +0100

    Covid publish with totals, daily figures in progress

--- covid/JS/covid.js
+++ covid/JS/covid.js
@@ -1,26 +1,19 @@
-function getDailyCase1() {
-  var date = new Date();
-  date.setDate(date.getDate()-1);
-  console.log(backDate(date));
+//Gets cumulative totals
+function getTotals() {
   var settings = {
     "async" : true,
     "crossDomain" : true,
+    "url" : "https://covid-19-data.p.rapidapi.com/country?format=json&name=uk",
     "method" : "GET",
-    "url" : "https:\//covid-19-data.p.rapidapi.com/report/country/code?format=json&date-format=YYYY-MM-DD&date=" + backDate(date) + "&code=gb",
     "headers" : {
-      "X-RapidAPI-Key" : "c6f23c4c02mshb135ec8895eeb2fp156352jsna8e730d46ed9",
-      "x-rapidapi-host" : "covid-19-data.p.rapidapi.com"
+      "x-rapidapi-host" : "covid-19-data.p.rapidapi.com",
+      "x-rapidapi-key" : "c6f23c4c02mshb135ec8895eeb2fp156352jsna8e730d46ed9"
     }
   }
-  $.ajax(settings).done(function (response) {
-    $.each(response.provinces, function(index, list) {
-      if (list.province == "UK") {
-        $('#caseData1').append(list.confirmed);
-        console.log("list: /n" + list.confirmed)
-        $('#caseDate1').append(datify(date));
-        console.log(date);
-      }
-    });
+  $.ajax(settings).done(function(response) {
+    console.log(response);
+    $('#caseTotal').append(response[0].confirmed);
+    $('#deathTotal').append(response[0].deaths);
   });
 }
 // --------------------------------
