290c2ba509d1682fbbb1019103fd6f94468d3306
Adam Crofts <adamcrofts@live.com>
2020-05-06 12:02:19 +0100

    Working example
    Shows latest totals vs 2 days ago. Either need to look at 1 day ago vs 2 days ago, or put an if statement.

--- covid/JS/covid.js
+++ covid/JS/covid.js
@@ -1,6 +1,15 @@
+var today = new Date();
+today.setDate(today.getDate()-2);
+var caseTotal = null;
+var deathTotal = null;
+var caseDaily = null;
+var deathDaily = null;
+var finalCase = null;
+var finalDeaths = null;
+
 //Gets cumulative totals
 function getTotals() {
-  var settings = {
+  var settingsToday = {
     "async" : true,
     "crossDomain" : true,
     "url" : "https://covid-19-data.p.rapidapi.com/country?format=json&name=uk",
@@ -10,11 +19,38 @@ function getTotals() {
       "x-rapidapi-key" : "c6f23c4c02mshb135ec8895eeb2fp156352jsna8e730d46ed9"
     }
   }
-  $.ajax(settings).done(function(response) {
+  $.ajax(settingsToday).done(function(response) {
     console.log(response);
     $('#caseTotal').append(response[0].confirmed);
+    caseTotal = response[0].confirmed;
     $('#deathTotal').append(response[0].deaths);
+    deathTotal = response[0].deaths;
   });
+}
+
+function getYesterday() {
+  console.log("getYesterday");
+  var settingsYesterday = {
+	   "async": true,
+	   "crossDomain": true,
+	   "url": "https://covid-19-data.p.rapidapi.com/report/country/code?format=json&date-format=YYYY-MM-DD&date=" + backDate(today) + "&code=gb",
+	   "method": "GET",
+	   "headers": {
+		     "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		     "x-rapidapi-key": "c6f23c4c02mshb135ec8895eeb2fp156352jsna8e730d46ed9"
+	      }
+      }
+  $.ajax(settingsYesterday).done(function(response) {
+    caseDaily = response[0].provinces[0].confirmed;
+    deathDaily = response[0].provinces[0].deaths;
+    finalCase = caseTotal - caseDaily;
+    console.log("finalCase: " + finalCase);
+    finalDeaths = deathTotal - deathDaily;
+    console.log("finalDeaths: " + finalDeaths);
+    $('#dailyCases').append(finalCase);
+    $('#dailyDeaths').append(finalDeaths);
+  });
+
 }
 // --------------------------------
 //
