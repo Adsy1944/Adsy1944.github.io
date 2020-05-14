3c8fed1bdaa5463779d8fc86fab90b8c7652307d
Adam Crofts <adamcrofts@live.com>
2020-05-14 17:00:01 +0100

    Working model!

--- covid/JS/covid.js
+++ covid/JS/covid.js
@@ -1,37 +1,56 @@
-var dayOne = new Date();
-dayOne.setDate(dayOne.getDate() -1);
-var dayTwo = new Date();
-dayTwo.setDate(dayTwo.getDate() -2);
-var caseTotal = null;
-var deathTotal = null;
-var caseDaily = null;
-var deathDaily = null;
-var finalCase = null;
-var finalDeaths = null;
+var totalCases = null;
+var totalDeaths = null;
 
 //Gets cumulative totals
 function getTotals() {
+  var date = null;
   var settingsToday = {
     "async": true,
-    "crossDomain": true,
-    "url": "https://covid-19-data.p.rapidapi.com/report/country/code?format=json&date-format=YYYY-MM-DD&date=" + backDate(dayOne) + "&code=gb",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-        "x-rapidapi-key": "c6f23c4c02mshb135ec8895eeb2fp156352jsna8e730d46ed9"
+	"crossDomain": true,
+	"url": "https://covid-19-data.p.rapidapi.com/country/code?format=json&code=gb",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "c6f23c4c02mshb135ec8895eeb2fp156352jsna8e730d46ed9"
        }
      }
   $.ajax(settingsToday).done(function(response) {
-    console.log(response);
-    $('#caseTotal').append(response[0].provinces[0].confirmed);
-    caseTotal = response[0].provinces[0].confirmed;
-    $('#deathTotal').append(response[0].provinces[0].deaths);
-    deathTotal = response[0].provinces[0].deaths;
+    $('#caseTotal').append(response[0].confirmed);
+    totalCases = response[0].confirmed;
+    $('#deathTotal').append(response[0].deaths);
+    totalDeaths = response[0].deaths;
+    date = response[0].lastChange;
+    setTimeout(function() {getDay(date)},1500);
   });
 }
 
+function getDay(date){
+  var newDate = new Date(date);
+  newDate.setDate(newDate.getDate() - 1);
+  var dailyCases = null;
+  var dailyDeaths = null;
+  var result = null;
+  var settingsDate = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://covid-19-data.p.rapidapi.com/report/country/code?format=json&date-format=YYYY-MM-DD&date=" + backDate(newDate) + "&code=gb",
+  "method": "GET",
+  "headers": {
+      "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+      "x-rapidapi-key": "c6f23c4c02mshb135ec8895eeb2fp156352jsna8e730d46ed9"
+     }
+  }
+  $.ajax(settingsDate).done(function(response) {
+    dailyCases = response[0].provinces[0].confirmed;
+    $('#dailyCases').append(totalCases - dailyCases);
+    dailyDeaths = response[0].provinces[0].deaths;
+    $('#dailyDeaths').append(totalDeaths - dailyDeaths);
+    $('#date').append(datify(date));
+  });
+}
+
+
 function getYesterday() {
-  console.log("getYesterday");
   var settingsYesterday = {
 	   "async": true,
 	   "crossDomain": true,
@@ -46,13 +65,10 @@ function getYesterday() {
     caseDaily = response[0].provinces[0].confirmed;
     deathDaily = response[0].provinces[0].deaths;
     finalCase = caseTotal - caseDaily;
-    console.log("finalCase: " + finalCase);
     finalDeaths = deathTotal - deathDaily;
-    console.log("finalDeaths: " + finalDeaths);
     $('#dailyCases').append(finalCase);
     $('#dailyDeaths').append(finalDeaths);
   });
-
 }
 // --------------------------------
 //
