d0cfac80f2ba2464925dbcca1aba7797aaab5aa7
Adam Crofts <adamcrofts@live.com>
2020-10-23 18:55:52 +0100

    Issue 6 posted

---
+++ covid/JS/covid2.js
@@ -0,0 +1,12 @@
+var totalCases = null;
+var totalDeaths = null;
+
+function getTotals() {
+  var date = null;
+  var settingsToday = {
+    'https://api.coronavirus.data.gov.uk/v1/data?' +
+    'filters=areaType=overview;&' +
+    'structure={"date":"date","newCases":"newCasesByPublishDate"}'
+  };
+
+}
